53d9fe4d86ede093e9495302e31db9fa9d826f54
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-10-12 00:00:47 -0400

    fix: refactor javascript

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var soundFile;
 
-function readFile(files) {
-    var fileReader = new FileReader();
-    fileReader.readAsArrayBuffer(files[0]);
+function readFile(soundFile) {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(soundFile);
     fileReader.onload = function(e) {
         playAudioFile(e.target.result);
-        console.log(("Filename: '" + files[0].name + "'"), ("(" + ((Math.floor(files[0].size / 1024 / 1024 * 100)) / 100) + " MB)"));
+        console.log(`Filename: '${soundFile.name}' (${(Math.floor(soundFile.size / 1024 / 1024 * 100)) / 100} MB)`);
     }
 }
 
 function playAudioFile(file) {
-    var context = new window.AudioContext();
+    const context = new window.AudioContext();
     context.decodeAudioData(file, function(buffer) {
-        var source = context.createBufferSource();
+        let source = context.createBufferSource();
         source.buffer = buffer;
         source.loop = false;
         source.connect(context.destination);
